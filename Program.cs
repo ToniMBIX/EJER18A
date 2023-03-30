4ad6523838f7c4adbb81eb4607883a8e4285eca8
ToniMBIX <antmarbou@alu.edu.gva.es>
2023-03-30 15:28:19 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class TriangleProgram
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Introduzca los tres lados del triángulo: ");
+
+        Console.Write("Lado 1: ");
+        double lado1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Lado 2: ");
+        double lado2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Lado 3: ");
+        double lado3 = double.Parse(Console.ReadLine());
+
+        if (lado1 == lado2 && lado1 == lado3)
+        {
+            Console.WriteLine("El triángulo es equilátero.");
+        }
+        else
+        {
+            double ladoMayor = Math.Max(lado1, Math.Max(lado2, lado3));
+            Console.WriteLine("El triángulo no es equilátero, el lado más largo es: " + ladoMayor);
+        }
+
+        Console.ReadKey();
+    }
+}
